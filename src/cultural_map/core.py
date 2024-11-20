3f8258b33b8ca75aa82493d957b7821588f2bc36
Impreccabme <evvlasov1990@gmail.com>
2024-11-20 17:03:03 +0100

    added visio structure .py

--- src/cultural_map/core.py
+++ src/cultural_map/core.py
@@ -6,12 +6,15 @@ from .components.ui import setup_page, create_sidebar, show_map
 from .components.map import create_map
 from .components.chat import process_chat_input
 
-
 @st.cache_data(show_spinner="Loading cultural data...")
 def load_data():
     """Load the preprocessed cultural data."""
     return pd.read_pickle("data/cultural_data.pkl")
 
+@st.cache_data
+def load_calculated_events_data():
+    """Load and cache the calculated events data."""
+    return pd.read_csv("data/results/nbre_equipement_parcommuneetcodepostal_ET_LatitudeLongitude_ET_POPULATION.csv", sep="\t")
 
 @st.cache_data(show_spinner=False)
 def filter_data(
@@ -38,7 +41,8 @@ def main():
     setup_page()
 
     # Load preprocessed data
-    data = load_data()
+    data = load_data()                                       ## pickle for the map
+    data_calculated_events = load_calculated_events_data()   ## csv for the visualisation
 
     # Create sidebar with filters and chat
     selected_categories, selected_types, selected_commune = create_sidebar(
@@ -52,6 +56,7 @@ def main():
     filtered_data = filter_data(
         data, selected_categories, selected_types, selected_commune
     )
+
 
     # Create map with filtered data
     filtered_map = create_map(
